6a3125c51a8fce20208d80ef41dcff8137e15769
Isaac <isaac_brown1@yahoo.com>
2025-07-30 16:35:32 -0400

    Initial commit: Add Project and tests

---
+++ nutrition_tracker.py
@@ -0,0 +1,144 @@
+import requests
+import re
+
+API_KEY = "C+dj44RzZvmMYGNERhe3lQ==2isRnHWv95uecohh"
+API_URL = "https://api.calorieninjas.com/v1/nutrition?query="
+
+class FoodItem:
+    """
+    Represents a food item with nutritional information.
+
+    Attributes:
+        name (str): Name of food item.
+        calories (flaot): Caloric content in kcal.
+        protein (float): Protein content in grams.
+        fat (flaot): Fat content in grams.
+        carbs (flaot): Carbohydrate content in grams.
+    """
+    def __init__(self, name, calories, protein, fat, carbs):
+        self.name = name
+        self.calories = calories
+        self.protein = protein
+        self.fat = fat
+        self.carbs = carbs
+
+    def __str__(self):
+        """Returns formatted string of the food item's nutritional content."""
+        if self.name == "total":
+            return (
+             f"\nCalories: {self.calories:.2f}kcal\n"
+             f"\nProtein: {self.protein:.2f}g\n"
+             f"\nFat: {self.fat:.2f}g\n"
+             f"\nCarbs: {self.carbs:.2f}g\n"
+        )
+        return (
+             f"\nName: {self.name}\n"
+             f"\nCalories: {self.calories:.2f}kcal\n"
+             f"\nProtein: {self.protein:.2f}g\n"
+             f"\nFat: {self.fat:.2f}g\n"
+             f"\nCarbs: {self.carbs:.2f}g\n"
+        )
+
+    def __add__(self, other):
+        """
+            Combines two FoodItem objects to return a new one with total values.
+
+            Attributes:
+                other (FoodItem): Another food item to combine.
+
+            Returns:
+                FoodITem: A new object with the combined nutritional info.
+        """
+        return FoodItem(
+            name = "total",
+            calories = self.calories + other.calories,
+            protein = self.protein + other.protein,
+            fat = self.fat + other.fat,
+            carbs = self.carbs + other.carbs,
+        )
+
+    @classmethod
+    def from_api_response(cls, query):
+        """
+            Creates a FoodItem from an API response.
+
+            Attributes:
+                query (str): User query (eg. 100g of chicken)
+
+            Returns:
+                FoodItem or None: A FoodItem object if successful, None otherwise.
+        """
+        url = API_URL + query
+        headers = {"X-Api-Key": API_KEY}
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            items = response.json().get("items", [])
+            if not items:
+                print(f"No Data Found. '{query}'")
+            item = items[0]
+            return cls(
+                        name=item.get("name", query),
+                        calories=item.get("calories", query),
+                        protein=item.get("protein_g", query),
+                        fat=item.get("fat_total_g", query),
+                        carbs=item.get("carbohydrates_total_g", query),
+                        )
+        except requests.exceptions.RequestException as err:
+            print(f"API Request Error: {err}")
+            return None
+
+
+def get_items():
+    """
+        Collects user input in the form of food and weight desctiptions.
+
+        Returns:
+            list: a list of valid food query strings
+    """
+    food_items = []
+    pattern = r"^\d*(?: ?(?:oz|g|lbs)? of)? \w+(?: \w+)*$"
+    while True:
+        try:
+            user_input = input("Food and weight: ")
+            if matches := re.fullmatch(pattern, user_input , re.IGNORECASE):
+                food_items.append(user_input)
+            else:
+                print("Invalid Format. Example: '20g of almonds'")
+        except EOFError:
+            break
+    return food_items
+
+def calculate_total(food_objects):
+    """
+        Sums a list of FoodItem objects into a single total.
+
+        Attributes:
+            food_objects (list): List of food item objects
+
+        Returns:
+            FoodItem: A new FoodItem with total values or None if empty.
+    """
+    if not food_objects:
+        return None
+    total = food_objects[0]
+    for item in food_objects[1:]:
+        total += item
+    return total
+
+
+def main():
+    """Main program loop. Collects input, queries API, prints totals"""
+    items_list = get_items()
+    food_objects = []
+    for item in items_list:
+        food = FoodItem.from_api_response(item)
+        if food:
+            food_objects.append(food)
+        total = calculate_total(food_objects)
+    if total:
+        print(f"\n==TOTAL NUTRITION=="
+              f"{total}")
+
+if __name__ == "__main__":
+    main()
---
+++ test_nutrition_tracker.py
@@ -0,0 +1,104 @@
+import unittest
+from unittest.mock import patch, Mock
+from nutrition_tracker import FoodItem, get_items, calculate_total
+
+
+class TestFoodItem(unittest.TestCase):
+    """Unit tests for the FoodItem class."""
+
+
+    def test_add_food_item(self):
+        """Test addition of two FoodItem instances"""
+        i1 = FoodItem("rice", 205, 4.3, 0.4, 44.5)
+        i2 = FoodItem("banana", 90, 1.1, 0.3, 23)
+        total = i1 + i2
+        self.assertEqual(total.calories, 295)
+        self.assertAlmostEqual(total.protein, 5.4)
+        self.assertEqual(total.fat, 0.7)
+        self.assertEqual(total.carbs, 67.5)
+
+    def test_str(self):
+        """Test string representation of a FoodItem"""
+        item = FoodItem("rice", 205, 4.3, 0.4, 44.5)
+        expected = (
+                f"\nName: rice\n"
+                f"\nCalories: 205.00kcal\n"
+                f"\nProtein: 4.30g\n"
+                f"\nFat: 0.40g\n"
+                f"\nCarbs: 44.50g\n"
+        )
+        self.assertEqual(str(item), expected)
+
+    def test_from_api_response_success(self):
+        """Test API response parseing when data is returnded."""
+        mock_response = {
+            "items": [
+                {
+                    "name": "apple",
+                    "calories": 52,
+                    "protein_g": 0.3,
+                    "fat_total_g": 0.2,
+                    "carbohydrates_total_g": 14,
+                }
+            ]
+        }
+
+        with patch("project.requests.get") as mock_get:
+            mock_get.return_value = Mock(status_code=200)
+            mock_get.return_value.json.return_value = mock_response
+            item = FoodItem.from_api_response("100g of apple")
+            self.assertEqual(item.name, "apple")
+            self.assertEqual(item.calories, 52)
+            self.assertAlmostEqual(item.protein, 0.3)
+            self.assertEqual(item.carbs, 14)
+
+    def test_from_api_response_no_data(self):
+        """Test API response with no data returned."""
+        with patch("project.requests.get") as mock_get:
+            mock_get.return_value = Mock(status_code=200)
+            mock_get.return_value.json.return_value = {"items": []}
+            item = FoodItem.from_api_response("nothing")
+            self.assertIsNone(item)
+
+
+class TestGetObjects(unittest.TestCase):
+    """Unit tests for the get_items function."""
+
+
+    def test_get_items_valid_then_EOF(self):
+        """Test valid input then EOF."""
+        user_inputs = ["100g of apples", EOFError]
+        with patch("builtins.input", side_effect=user_inputs):
+            objects = get_items()
+            self.assertEqual(objects, ["100g of apples"])
+
+    def test_get_items_invalid_then_EOF(self):
+        """"Test invalid input then EOF."""
+        user_inputs = ["apple 100g", EOFError]
+        with patch("builtins.input", side_effect=user_inputs):
+            objects = get_items()
+            self.assertEqual(objects, [])
+
+
+class TestCalculateTotal(unittest.TestCase):
+    """Unit tests for the calculate_total function."""
+
+
+    def test_calculate_total(self):
+        """Test summing multiple FoodItem objects"""
+        i1 = FoodItem("rice", 205, 4.3, 0.4, 44.5)
+        i2 = FoodItem("banana", 90, 1.1, 0.3, 23)
+        total = calculate_total([i1, i2])
+        self.assertEqual(total.calories, 295)
+        self.assertAlmostEqual(total.protein, 5.4)
+        self.assertEqual(total.fat, 0.7)
+        self.assertEqual(total.carbs, 67.5)
+
+    def test_calculate_total_empty(self):
+        """Test summing empty list returns None"""
+        total = calculate_total(None)
+        self.assertEqual(total, None)
+
+
+if __name__ == "__main__":
+    unittest.main()
